35fd20ebef299c8b4759331414d87d3b1f893415
chinni00 <chinni0437@gmail>
2023-11-25 13:17:45 +0530

    added filter for routes

--- first-app.js
+++ first-app.js
@@ -5,7 +5,7 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 const parser = require("body-parser");
 app.use(parser.urlencoded({extended:true}));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes)
 app.use((req,res,next)=>{
     res.status(404).send('<center><h1>Page Not Found</h1></center>')
