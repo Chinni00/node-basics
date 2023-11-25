703535c725fe92b666ff77a289d0e7a393e6f8ad
chinni00 <chinni0437@gmail>
2023-11-25 13:05:40 +0530

    error page created

--- first-app.js
+++ first-app.js
@@ -7,6 +7,9 @@ const parser = require("body-parser");
 app.use(parser.urlencoded({extended:true}));
 app.use(adminRoutes);
 app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send('<center><h1>Page Not Found</h1></center>')
+})
 
 
 app.listen(3000);
